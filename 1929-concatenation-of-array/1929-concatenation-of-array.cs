6c06bc6d81ea9a75e86579aa878e72a76c07206e
beso_zain <139134291+besozain@users.noreply.github.com>
2025-05-27 22:34:25 +0300

    Time: 1 ms (36.56%), Space: 51.7 MB (76.09%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -1,12 +1,21 @@
 public class Solution {
     public int[] GetConcatenation(int[] nums) {
+        int roundStart = 0;
         int round = 2;
         int start = 0;
+        bool flag = true;
         int[] result = new int[nums.Length * 2];
-        for(int i = 0 ; i < round; i++) {
-            for(int j = 0 ; j < nums.Length; j++) {
-                result[start] = nums[j];
-                start++;
+        for(int i = 0 ; i < nums.Length; i++) {
+            if(flag == true) {
+            result[start] = nums[i];
+            start++;
+            if(i ==  nums.Length-1 ) {
+                roundStart++;
+                i = -1;
+                if(roundStart == round){
+                    flag = false;
+                }
+            }
             }
         }
         return result;
