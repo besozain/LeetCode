acc7ce95d820850f1f5e4f84b4e0318149a4d380
beso_zain <139134291+besozain@users.noreply.github.com>
2025-05-29 22:20:25 +0300

    Time: 9 ms (14%), Space: 40.5 MB (30.46%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if( haystack.Contains(needle))
+ {
+     return haystack.IndexOf(needle);
+ }
+ return -1;
+    }
+}
