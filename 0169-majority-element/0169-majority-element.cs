450b94f801bd21d681398b0519569b4fa693ec43
beso_zain <139134291+besozain@users.noreply.github.com>
2025-07-06 18:44:40 +0300

    Time: 21 ms (16.32%), Space: 50 MB (32.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,29 +1,5 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
         Array.Sort(nums);
-int avarage = nums.Length / 2;
-int count = 0;
-int result = 0;
-if(nums.Length == 1) {
-    return nums[0] ;
-}
-
-for (int i = 0; i < nums.Length; i++)
-{
-    if(i < nums.Length -1 && nums[i] == nums[i + 1] )
-    {
-        count++;
-        if (count >= avarage)
-        {
-            result = nums[i];
-        }
-    }
-    else
-    {
-        count = 0;
-    }
-
-}
-return result;
-    }
+    return nums[nums.Length / 2];}
 }
