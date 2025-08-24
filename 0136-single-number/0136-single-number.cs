fcec2a29ead45806cc1bcbaff98494b0fa543ae8
beso_zain <139134291+besozain@users.noreply.github.com>
2025-08-24 11:12:14 +0300

    Time: 25 ms (16.33%), Space: 46.9 MB (43.89%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,31 +1,18 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
-        if (nums.Length == 1)
+        Array.Sort(nums);
+        for(int i = 0; i < nums.Length; i += 2 )
         {
-            return nums.First();
-        }
-        List<int> ints = nums.ToList();
-        int test = ints[0];
-        int count = 0;
-        for(int i = 0; i < ints.Count; i++)
-        {
-            if (test == ints[i])
+            if(i == nums.Length - 1)
             {
-                count++;
-                if (count == 2)
-                {
-                    ints.RemoveAll(s => s == test);
-                    count = 0;
-                    i = -1;
-                } 
-                if(ints.Count == 1)
-                {
-                    return ints[0];
-                }
-                test = ints[0];
+                return nums[i];
             }
-
+            if (nums[i] != nums[i+1])
+            {
+                return nums[i];
+            }
+        
         }
-        return test;
+        return 0;
     }
 }
